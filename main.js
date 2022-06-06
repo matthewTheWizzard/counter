60baa29a18b43f62f488ea227a5bf749ea0253f5
matthewTheWizzard <mmdrumnew@gmail.com>
2022-06-06 09:10:02 +0300

    trying new method

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
+// first way to solve the problem
+
 const decrease = document.querySelector('.decrease');
 const reset = document.querySelector('.reset');
 const increase = document.querySelector('.increase');
 let value = document.querySelector('.value');
+
 
 value.textContent = 0;
 decrease.addEventListener('click', function(){
@@ -18,3 +21,22 @@ reset.addEventListener('click', function(){
     
 });
 
+// second way to solve problem
+
+/*
+const buttons = function(){
+    const buttons = document.querySelectorAll('.btn');
+    console.log(buttons);
+    let buttonsArray = [];
+    for(let i = 0; i < 3; i++){
+        let button = buttons[i];
+        console.log(button);    
+        buttonsArray.push(button);
+        console.log(buttonsArray);
+    }
+    console.log(buttonsArray);
+    return buttonsArray;
+};
+
+
+*/
